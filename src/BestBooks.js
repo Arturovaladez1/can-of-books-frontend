082d036f59952d854a554ef2ad6fc7c70cfc70cd
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-03-14 22:18:44 -0700

    still working on the trello cards, 60% done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
+import BookForm from './Form'
+import Button from 'react-bootstrap/Button';
+import DeleteButton from './DeleteButton'
+
 
 let SERVER = process.env.REACT_APP_SERVER;
 
@@ -9,12 +13,58 @@ class BestBooks extends React.Component {
     super(props);
     this.state = {
       books: [],
-      showModal: false,
-      error: {}
+      modalOpen: false,
     }
   }
 
+  handleModalOpen = () => {
+    this.setState({
+      modalOpen: true,
+    })
+  }
+
+  handleModalClose = () => {
+    this.setState({
+      modalOpen: false
+    })
+  }
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+
+
+  // create a book
+  addBook = async (input) => {
+    try {
+      let url = `${process.env.SERVER}/books`;
+      let addedBook = await axios.post(url, input);
+
+      this.setState({
+        books: [...this.state.books, addedBook.data]
+      })
+    } catch (error) {
+      console.log(error.response.data);
+      this.setState({
+        error: error
+      });
+    }
+  }
+  handleDelete = async (id) => {
+    await this.deleteBook(id)
+  }
+  deleteBook = async (id) => {
+    try {
+      console.log("clicked")
+      let url = `${SERVER}/books/book/${id}`
+      await axios.delete(url);
+      let updatedBooks = this.state.books.filter(book => book._id !== id);
+      this.setState({
+        books: updatedBooks
+      });
+
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
   getBooks = async () => {
     try {
       let results = await axios.get(`${SERVER}/books`);
@@ -26,23 +76,6 @@ class BestBooks extends React.Component {
     }
 
   }
-
-  addBook = async (input) => {
-    try {
-      let url = `${process.env.SERVER}/books`;
-      let addedBook = await axios.post(url, input);
-      
-      this.setState({
-        books: [...this.state.books, addedBook.data]
-      })
-    } catch (error) {
-      console.log(error.response.data);
-      this.setState({
-        error: error
-      });
-    }
-  }
-
 
   componentDidMount() {
     // console.log(SERVER);
@@ -66,6 +99,9 @@ class BestBooks extends React.Component {
           <h2> {book.title}</h2>
           <p>{book.description}</p>
         </Carousel.Caption>
+       <div className='DeleteButtonDiv'> 
+       <DeleteButton onClick= {() => {this.deleteBook(book._id)}}/>
+         </div>
       </Carousel.Item>
     })
 
@@ -74,13 +110,20 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
+          <>
+            <Carousel>
+              {books}
+            </Carousel>
 
-          <Carousel>
-            {books}
-          </Carousel>
+            <Button onClick={this.handleOpenModal}>Add A Book</Button>
+            <BookForm addBook={this.createBook}
+              show={this.state.modalOpen}
+              handleClose={this.handleCloseModal} />
+          </>
         ) : (
           <h3>No Books Found </h3>
         )}
+
       </>
     )
   }
